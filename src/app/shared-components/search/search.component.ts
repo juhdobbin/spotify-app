3433f75034829f637a26bb25cbe6cc62d6407775
Juliana Dobbin <juhfernandes@outlook.com>
2020-08-10 21:00:20 -0300

    layout responsivo

--- src/app/shared-components/search/search.component.ts
+++ src/app/shared-components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output } from '@angular/core';
+import { Component, Output, OnInit } from '@angular/core';
 import { SearchService } from '../../services/search.service';
 import { debounceTime, distinctUntilChanged } from 'rxjs/internal/operators';
 
@@ -7,7 +7,7 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/internal/operators';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   @Output() resultSearch;
 
   public searchValue;
@@ -16,16 +16,24 @@ export class SearchComponent {
     this.searchService.inputSearchChanged$
       .pipe(debounceTime(500), distinctUntilChanged())
       .subscribe(() => {
-        if (!this.searchValue) {
-          this.searchService.searchResults$.next(null);
-        } else {
+        if (this.searchValue && this.searchValue !== '') {
           this.searchValue = this.searchService.inputSearchChanged$.value;
           this.search(this.searchValue);
+        } else {
+          // limpa a lista de busca ao apagar os caracteres do input
+          this.searchService.searchResults$.next(null);
         }
       });
   }
+  ngOnInit() {
+    this.searchValue = this.searchService.inputSearchChanged$.value;
+  }
 
   search(value) {
+    if (!value) {
+      return;
+    }
+
     this.searchService.search(value).subscribe(
       (res) => this.searchService.searchResults$.next(res),
       (err) => console.log(err)
